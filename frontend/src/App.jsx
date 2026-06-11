9ae7fb2e9ef4d10ad933476f2d3400bf281f6ad3
jannat310 <jannat14101993@gmail.com>
2026-06-11 12:20:03 +0500

    fix wildcard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import {Routes, Route, Navigate} from 'react-router-dom'  
-import HomePage from './pages/HomePage'
-import SignUpPage from './pages/SignUpPage'
-import LoginPage from './pages/LoginPage'
-import SettingsPage from './pages/SettingsPage'
-import ProfilePage from './pages/ProfilePage' 
-import { useAuthStore } from './store/useAuthStore'
-import { useEffect } from 'react'
-import { Loader } from "lucide-react"
-import { Toaster } from 'react-hot-toast'
-import {useThemeStore} from './store/useThemeStore'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
-
-  const{authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore();
-  const{theme}  = useThemeStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
   console.log("Online users:", onlineUsers);
 
@@ -24,30 +22,40 @@ const App = () => {
     checkAuth();
   }, [checkAuth]);
 
-  if(isCheckingAuth && !authUser)
+  if (isCheckingAuth && !authUser)
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
       </div>
     );
-  
 
   return (
-    <div data-theme={theme} >
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
-        <Route path = "/" element= {authUser ? <HomePage/> : <Navigate to="/login" />} />
-        <Route path = "/signUp" element = {!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
-        <Route path = "/login" element = {!authUser ?<LoginPage/> : <Navigate to="/"/>}/>
-        <Route path = "/settings" element = { <SettingsPage/> } />
-        <Route path = "/profile" element = {authUser ? <ProfilePage/> : <Navigate to="/login" />} />
-
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signUp"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
 
       <Toaster />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
