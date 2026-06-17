379835f2730a2c844f3eddcb57547d38e02a2bb2
jannat310 <jannat14101993@gmail.com>
2026-06-17 14:13:57 +0500

    Remove wrong Import

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@
 import {create} from 'zustand'  
 import { axiosInstance } from '../lib/axios.js'
 import toast from 'react-hot-toast';
-// import { login } from '../../../backend/src/controllers/auth.controller';
+import { login } from '../../../backend/src/controllers/auth.controller';
 import { io } from 'socket.io-client';
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/"; 
